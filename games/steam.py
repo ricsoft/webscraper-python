3ecc6b6a496a4faa2ed312446e289c121306c339
Richard <42780925+ricsoft@users.noreply.github.com>
2023-07-12 20:41:32 -0700

    Update steam

--- games/steam.py
+++ games/steam.py
@@ -4,14 +4,14 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 from helpers.games import *
 
-url = "https://store.steampowered.com/"
+url = "https://store.steampowered.com/specials/?flavor=contenthub_newandtrending"
 site = {"site": "steam"}
-specials_tab_xpath = "//div[@id='tab_specials_content_trigger']"
-games_div_xpath = "//div[@id='tab_specials_content']"
-game_image_xpath = "//div[@id='tab_specials_content']//img[@class='tab_item_cap_img']"
-game_title_xpath = "//div[@id='tab_specials_content']//div[@class='tab_item_name']"
-game_price_xpath = "//div[@id='tab_specials_content']//div[@class='discount_original_price']"
-game_sale_price_xpath = "//div[@id='tab_specials_content']//div[@class='discount_final_price']"
+games_div_xpath = "//div[@class='facetedbrowse_FacetedBrowseItems_NO-IP']"
+game_image_xpath = games_div_xpath + "//div[contains(@class, 'salepreviewwidgets_HeaderCapsuleImageContainer_" \
+                                     "')]//img[contains(@class, 'salepreviewwidgets_CapsuleImage_')]"
+game_title_xpath = games_div_xpath + "//div[contains(@class, 'salepreviewwidgets_StoreSaleWidgetTitle_')]"
+game_price_xpath = games_div_xpath + "//div[contains(@class, 'salepreviewwidgets_StoreOriginalPrice_')]"
+game_sale_price_xpath = games_div_xpath + "//div[contains(@class, 'salepreviewwidgets_StoreSalePriceBox_')]"
 
 
 def cleanup_games(game_titles, game_images, game_prices, game_sale_prices):
@@ -21,22 +21,20 @@ def cleanup_games(game_titles, game_images, game_prices, game_sale_prices):
             titles_to_keep.append(title.text)
 
     game_titles[:] = list(titles_to_keep)
+    del game_images[1::2]
     check_count([game_titles, game_images, game_prices, game_sale_prices])
 
     for i in range(len(game_titles)):
         game_images[i] = game_images[i].get_attribute("src").split("?", 1)[0]
-        game_prices[i] = f"${game_prices[i].text.replace('CDN$ ', '')}"
-        game_sale_prices[i] = f"${game_sale_prices[i].text.replace('CDN$ ', '')}"
+        game_prices[i] = f"${game_prices[i].text.replace('C$ ', '')}"
+        game_sale_prices[i] = f"${game_sale_prices[i].text.replace('C$ ', '')}"
 
 
 def scrape_steam(webdriver, db):
     try:
         webdriver.get(url)
         WebDriverWait(webdriver, 5).until(
-            ec.element_to_be_clickable((By.XPATH, specials_tab_xpath))
-        ).click()
-        WebDriverWait(webdriver, 5).until(
-            ec.visibility_of_element_located((By.XPATH, games_div_xpath))
+            ec.visibility_of_element_located((By.XPATH, game_image_xpath))
         )
 
         games_div = webdriver.find_element(By.XPATH, games_div_xpath)
